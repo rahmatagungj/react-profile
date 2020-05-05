29ccba3d9c5e4fea3ea376b1cbdec4a3973f2872
rahmatagungj <rahmatagungj@gmail.com>
2020-05-05 14:01:43 +0700

    Updates

---
+++ precache-manifest.55de5b303d1e0b17e0ce3618de18d817.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5805607f1c4363e595af4f2a281fc08",
+    "url": "/halo/index.html"
+  },
+  {
+    "revision": "2b2426e7162994687fb9",
+    "url": "/halo/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "98f9bf9f81e832ebf7b5",
+    "url": "/halo/static/css/main.c5558e78.chunk.css"
+  },
+  {
+    "revision": "2b2426e7162994687fb9",
+    "url": "/halo/static/js/2.0f185555.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/halo/static/js/2.0f185555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f9bf9f81e832ebf7b5",
+    "url": "/halo/static/js/main.0f85905e.chunk.js"
+  },
+  {
+    "revision": "08907702f42f6061f4f3",
+    "url": "/halo/static/js/runtime-main.711b8aa9.js"
+  },
+  {
+    "revision": "4b128abe38d674b3e5d1a79a2b4e67a6",
+    "url": "/halo/static/media/foto.4b128abe.png"
+  }
+]);
